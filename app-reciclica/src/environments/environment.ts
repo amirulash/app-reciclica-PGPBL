f7c4c3722683c26829765b68c4c53c3b161e0101
amirulash <amirulfahmiash-shiddiqie@mail.ugm.ac.id>
2024-09-01 23:16:38 +0700

    Acara 2

---
+++ app-reciclica/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBL83oXCmkAAUdJl84cdCDwXV5L8t4mgWU",
+    authDomain: "reciclica-a4545.firebaseapp.com",
+    projectId: "reciclica-a4545",
+    storageBucket: "reciclica-a4545.appspot.com",
+    messagingSenderId: "288391047909",
+    appId: "1:288391047909:web:acb87e3cccd48d89f5420c"
+  }
+};
